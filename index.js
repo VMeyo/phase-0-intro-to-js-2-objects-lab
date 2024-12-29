b756593eb8cb5fba56aa1383065682c89d8fb76a
Victor Meyo <victor.meyo@student.moringaschool.com>
2024-12-29 21:19:50 +0300

    Assignment on objects completed

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution in this file!
+
+//Updating employee with key and value
+const employee = {};
+
+function updateEmployeeWithKeyAndValue(employee, name, Sam){
+
+const newEmployee = {...employee};
+
+newEmployee[name] = Sam;
+
+return newEmployee;
+
+}
+
+
+//Destructively Updating employee with key and value
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, Sam){
+    
+    employee[name] = Sam;
+
+    return employee;
+
+    const newemployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Victor");
+}
+    
+
+//Delete from employee by Key
+
+function deleteFromEmployeeByKey(employee, name ){
+
+    const newEmployee = {...employee};
+
+    delete newEmployee[name];
+
+    return newEmployee;
+}
+
+//Destructively delete from employee by key
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    
+    delete employee[name];
+  
+    return employee;
+  }
